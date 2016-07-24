c08fbafb4e6e74721de25b8a92f42a69d69d3027
valentinebm <valentine.barbierm@gmail.com>
2016-07-24 21:08:59 +0200

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :reigns
+  
   def self.create_with_omniauth(auth)
 
     user = find_or_create_by(uid: auth['uid'], provider: auth['provider'])
--- db/migrate/20160723210812_add_columns_to_users.rb
+++ db/migrate/20160723210812_add_columns_to_users.rb
@@ -3,5 +3,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :first_name, :string
+    add_column :users, :password, :string
   end
 end
