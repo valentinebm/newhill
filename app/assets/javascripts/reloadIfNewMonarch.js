b63c25f2ddb3f45d06b99ebe5936d1b515b3a160
valentinebm <valentine.barbierm@gmail.com>
2016-08-01 18:07:11 +0200

    assets

--- app/assets/javascripts/reloadIfNewMonarch.js
+++ app/assets/javascripts/reloadIfNewMonarch.js
@@ -2,7 +2,6 @@ function reloadIfNewMonarch(user_id){
 
   setInterval(function(){
     $.get( "/"+user_id+"/check_monarch", function( data ) {
-      // console.log(data)
       console.log(data);
       if (data === false) {
         window.location.reload();
