bba5445a08ee332c0125a8d6f45c865eb3e73815
Valentine Barbier-Mueller <valentine.barbierm@gmail.com>
2016-08-03 00:19:37 +0200

    history lighter

--- app/assets/javascripts/reloadIfNewMonarch.js
+++ app/assets/javascripts/reloadIfNewMonarch.js
@@ -2,12 +2,11 @@ function reloadIfNewMonarch(user_id){
 
   setInterval(function(){
     $.get( "/"+user_id+"/check_monarch", function( data ) {
-      console.log(data);
       if (data === false) {
         window.location.reload();
       }
 
-      });
+    });
   }, 10000)
 
 }
