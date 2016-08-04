a021045c3bd5d3a5b4cc28fc7a837e6194ead20d
Valentine Barbier-Mueller <valentine.barbierm@gmail.com>
2016-08-04 20:20:02 +0200

    random gif

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,11 @@ class SessionsController < ApplicationController
   def create
     if request.env['omniauth.auth']
       user = User.create_with_omniauth(request.env['omniauth.auth'])
-      # unless user.id == current_monarch.id
-      #   Reign.transition(user.id)
-      # end
       session[:user_id] = user.id
       redirect_to users_path
     else
       user = User.find_by_email(params[:email])
       user && user.authenticate(params[:password])
-      # unless user.id == current_monarch.id
-      #   Reign.transition(user.id)
-      # end
       session[:user_id] = user.id
       redirect_to users_path
     end
--- app/helpers/reigns_helper.rb
+++ app/helpers/reigns_helper.rb
@@ -1,2 +1,5 @@
 module ReignsHelper
+  def random_number
+    rand(1...8)
+  end
 end
