b5d78b306a865965cc3e1d2c169e19e30292cf8f
Valentine Barbier-Mueller <valentine.barbierm@gmail.com>
2016-08-04 19:31:14 +0200

    some changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,6 +38,23 @@ $(function () {
 //   })
 // }
 
+  var scroll_start = 0;
+  var startchange = $('.page-top');
+  var offset = startchange.offset();
+  $(document).scroll(function(){
+
+    scroll_start = $(this).scrollTop();
+    if(scroll_start > offset.top) {
+      $('nav').css("background-color", "rgba(253, 253, 253, 1)");
+      $('nav').css("box-shadow", "0 2px 4px rgba(0, 0, 0, 0.17)");
+    }
+      else {
+        $('nav').css('background-color', "transparent");
+        $('nav').css("box-shadow", "");
+      }
+
+  })
+
 
 // moveBackAndForth($('.cloud-1'), 5000)
 // moveBackAndForth($('.cloud-2'), 6000)
