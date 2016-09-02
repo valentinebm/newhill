e10a03e46c4c07d4eb3d3ed50223da3afa9c1df3
Chris Piazza <piazza.chris@gmail.com>
2016-09-02 08:10:20 -0400

    ensures a valid reign before proceedingwith user updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,20 @@ class UsersController < ApplicationController
   end
 
   def update
-    @previous_reign = Reign.last
-    @previous_reign.finished = DateTime.now
-    @previous_reign.duration = @previous_reign.finished - @previous_reign.created_at
-    @previous_reign.save
+    previous_reign = Reign.last
 
-    User.find(@previous_reign.user_id).update_total_reign(@previous_reign.duration)
+    begin
+      @reign = Reign.create!(user_id: current_user.id, weapon: params[:reign][:weapon])
+    rescue Exception => e
+      flash[:message] = e.message
+      redirect_to users_path
+    end
 
-    @reign = Reign.create(user_id: current_user.id, weapon: params[:reign][:weapon])
+    previous_reign.finished = DateTime.now
+    previous_reign.duration = previous_reign.finished - previous_reign.created_at
+    previous_reign.save
 
+    User.find(previous_reign.user_id).update_total_reign(previous_reign.duration)
     redirect_to users_path
   end
 
